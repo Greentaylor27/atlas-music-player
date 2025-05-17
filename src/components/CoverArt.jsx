e4dab0664abebdd0f3b86566ddb3574259234818
Greentaylor27 <greentaylor18@gmail.com>
2025-05-17 18:22:10 -0500

    Adding some basic components to the project so far it's looking Good!!

---
+++ src/components/CoverArt.jsx
@@ -0,0 +1,9 @@
+import placeholder from "../assets/placeholder.svg"
+
+export default function CoverArt() {
+    return(
+        <div>
+            <img src={placeholder} alt="Placeholder image" className="flex max-w-[400px] max-h-[400px]" />
+        </div>
+    )
+}
---
+++ src/components/SongTitle.jsx
@@ -0,0 +1,8 @@
+export default function SongTitle() {
+    return (
+        <div>
+            <h1>Tidal Drift</h1>
+            <p>Echoes of the Sea</p>
+        </div>
+    )
+}
