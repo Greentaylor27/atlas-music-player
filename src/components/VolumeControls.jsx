cda9d5da129a9d18fdcb3fe3ae74e880835b7b57
Greentaylor27 <greentaylor18@gmail.com>
2025-05-19 10:15:38 -0500

    Using a placeholder for the slider will work on it more later need to get the rest done though

--- src/components/VolumeControls.jsx
+++ src/components/VolumeControls.jsx
@@ -1,4 +1,5 @@
 import volumeIcon from "../assets/VolumeIcon.svg";
+import slider from "../assets/Slider.svg";
 import React, { useState } from "react";
 
 export default function VolumeControls() {
@@ -7,11 +8,18 @@ export default function VolumeControls() {
     const handleVolumeChange = (event) => {
         setVolume(Number(event.target.value));
     }
-    
+
     return(
+        <div className="flex max-w-xl items-center">
+            <img src={volumeIcon} alt="Volume Icon" className="pr-1"/>
+            <img src={slider} alt="Volume Slider" className="h-3.5" />
+        </div>
+    )
+    
+    { /* return(
         <div className="flex justify-between max-w-xl">
             <img src={volumeIcon} alt="Volume Icon" />
             <input type="range" min="0" max="100" step="1" value={volume} onChange={handleVolumeChange} />
         </div>
-    );
+    ); */ }
 }
