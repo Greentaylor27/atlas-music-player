da800e2b865abf82496d3c1f5f7b0481bbe234d8
Greentaylor27 <greentaylor18@gmail.com>
2025-05-20 21:02:25 -0500

    okay went back and made the coverart move responsive

--- src/components/CoverArt.jsx
+++ src/components/CoverArt.jsx
@@ -2,8 +2,11 @@ import placeholder from "../assets/placeholder.svg"
 
 export default function CoverArt() {
     return(
-        <div>
-            <img src={placeholder} alt="Placeholder image" className="" />
+        <div className="w-full max-w-sm aspect-square mx-auto rounded-xl overflow-hidden bg-gray-100">
+            <img 
+            src={placeholder}
+            alt="Placeholder image"
+            className="object-cover w-full h-full" />
         </div>
     )
 }
