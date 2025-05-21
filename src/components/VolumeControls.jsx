6347aa356f57d7c20dc25ee62532e3d8389c822d
Greentaylor27 <greentaylor18@gmail.com>
2025-05-20 21:30:45 -0500

    So adjust volume control a bit to look like what the project is asking RATHER than the figma I will style it more later

--- src/components/VolumeControls.jsx
+++ src/components/VolumeControls.jsx
@@ -3,23 +3,34 @@ import slider from "../assets/Slider.svg";
 import React, { useState } from "react";
 
 export default function VolumeControls() {
-    const [volume, setVolume] = useState(0); // Intial Volume
+    const [volume, setVolume] = useState(0);
 
     const handleVolumeChange = (event) => {
         setVolume(Number(event.target.value));
-    }
+    };
 
-    return(
-        <div className="flex max-w-xl items-center">
-            <img src={volumeIcon} alt="Volume Icon" className="pr-1"/>
-            <img src={slider} alt="Volume Slider" className="h-3.5" />
+  return (
+    <div className="flex items-center w-full max-w-sm">
+      <img src={volumeIcon} alt="Volume Icon" className="w-5 h-5" />
+      <img
+        src={slider}
+        alt="Volume Slider"
+        className="w-full h-3 object-contain -ml-10 rounded-full"
+      />
+    </div>
+  )
+
+    {/* return (
+        <div className="flex items-center space-x-3 w-full max-w-sm">
+            <img src={volumeIcon} alt="Volume Icon" className="w-5 h-5" />
+            <input
+                type="range"
+                min="0"
+                max="100"
+                value={volume}
+                onChange={handleVolumeChange}
+                className="w-full h-1 bg-gray-300 rounded-lg appearance-none cursor-pointer accent-indigo-500"
+            />
         </div>
-    )
-    
-    { /* return(
-        <div className="flex justify-between max-w-xl">
-            <img src={volumeIcon} alt="Volume Icon" />
-            <input type="range" min="0" max="100" step="1" value={volume} onChange={handleVolumeChange} />
-        </div>
-    ); */ }
+    ) */}
 }
