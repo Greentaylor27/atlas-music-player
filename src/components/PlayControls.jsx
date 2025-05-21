46d9ebf2d59c17f3fcfc3b587d41844cf6bcbb93
Greentaylor27 <greentaylor18@gmail.com>
2025-05-21 13:13:32 -0500

    updated voerart to not have a max size so that way it fits the figma more closely.
    Updated playcontrols to switch between pause and play because it bothered me pause wasn't being used.

--- src/components/PlayControls.jsx
+++ src/components/PlayControls.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import shuffle from "../assets/ShuffleIcon.svg";
 import back from "../assets/BackIcon.svg";
 import play from "../assets/PlayButton.svg";
@@ -5,19 +6,23 @@ import pause from "../assets/PauseButton.svg";
 import skip from "../assets/SkipIcon.svg"
 
 export default function PlayControls() {
+    const [isPlaying, setIsPlaying] = useState(true);
+
     return(
         <div className="flex flex-row justify-between max-w-xl">
-            <p className="flex items-center text-lg">1x</p>
-            <button>
+            <button className="cursor-pointer">
+                <p className="flex items-center text-lg">1x</p>
+            </button>
+            <button className="cursor-pointer">
                 <img src={back} alt="Back Button" />
             </button>
-            <button>
-                <img src={play} alt="Play Button" />
+            <button className="cursor-pointer" onClick={() => setIsPlaying(prev => !prev)}>
+                <img src={isPlaying ? pause : play} alt="Play/Pause Button" />
             </button>
-            <button>
+            <button className="cursor-pointer">
                 <img src={skip} alt="Skip Button" />
             </button>
-            <button>
+            <button className="cursor-pointer">
                 <img src={shuffle} alt="Shuffle Button" />
             </button>
         </div>
