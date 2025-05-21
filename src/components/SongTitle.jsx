e0e4ab727eecac60f266e188baa42f62b4540ed7
Greentaylor27 <greentaylor18@gmail.com>
2025-05-20 20:52:18 -0500

    Restyled song title to more closely match figma file

--- src/components/SongTitle.jsx
+++ src/components/SongTitle.jsx
@@ -1,8 +1,8 @@
 export default function SongTitle() {
     return (
         <div>
-            <h1>Tidal Drift</h1>
-            <p>Echoes of the Sea</p>
+            <h1 className="text-2xl font-bold pb-0.5">Tidal Drift</h1>
+            <p className="text-gray-400 text-base">Echoes of the Sea</p>
         </div>
     )
 }
