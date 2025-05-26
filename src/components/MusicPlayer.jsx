de642ce83c597bd558291af048e0512b267a24a0
Greentaylor27 <greentaylor18@gmail.com>
2025-05-26 13:18:58 -0500

    removing the bored on the app

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -5,10 +5,10 @@ import Playlist from "./Playlist";
 export default function MusicPlayer() {
   return(
     <div className="flex flex-col md:flex-row gap-8 p-4 mx-auto h-full w-full">
-      <div className="flex-1 border-2 border-fist1-200">
+      <div className="flex-1 border-2">
         <CurrentlyPlaying />
       </div>
-      <div className="flex-1 border-2 border-fist1-200">
+      <div className="flex-1">
         <Playlist />
       </div>
     </div>
